b712e5509c7fa50cc234f5c2c005a7991fa20a45
Binoy Saha <binoy.saha01@northsouth.edu>
2022-05-26 23:24:14 +0600

    all done

---
+++ script.js
@@ -0,0 +1,34 @@
+const s_color = document.querySelector('div');
+const btn=document.querySelectorAll('button');
+
+btn[0].addEventListener('click',function(){
+    s_color.style.backgroundColor='violet';
+})
+ 
+btn[1].addEventListener('click',function(){
+    s_color.style.backgroundColor='blue';
+})
+
+btn[2].addEventListener('click',function(){
+    s_color.style.backgroundColor='indigo';
+})
+
+btn[3].addEventListener('click',function(){
+    s_color.style.backgroundColor='green';
+})
+
+btn[4].addEventListener('click',function(){
+    s_color.style.backgroundColor='yellow';
+})
+
+btn[5].addEventListener('click',function(){
+    s_color.style.backgroundColor='orange';
+})
+
+btn[6].addEventListener('click',function(){
+    s_color.style.backgroundColor='red';
+})
+
+btn[7].addEventListener('click',function(){
+    s_color.style.backgroundColor='pink';
+})
